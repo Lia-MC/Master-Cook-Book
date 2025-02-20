ec113bb6a0f23a1c9b002a1fc812e1a0031bfce6
Lia-MC <lia.mc73700@gmail.com>
2025-02-19 21:56:29 -0500

    Added terminal outputs in main

--- main.py
+++ main.py
@@ -2,3 +2,7 @@
 # ask for preferences
 # neural network layers
 # pass out an answer through a cute chatbot interface
+
+def main():
+    print("Welcome to the MASTER COOK BOOK!!!")
+    # use streamlit?
