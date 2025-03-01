689d384f9475739a178a26a35bd2951bd4d54f77
Lia-MC <lia.mc73700@gmail.com>
2025-03-01 17:32:38 -0500

    Added streamlit to main. Not sure why but it's not displaying the buttons when I try to run streamlit...

--- main.py
+++ main.py
@@ -3,6 +3,14 @@
 # neural network layers
 # pass out an answer through a cute chatbot interface
 
+import streamlit as st
+
+st.set_page_config(page_title="Master Cook Book", layout="centered")
+
 def main():
-    print("Welcome to the MASTER COOK BOOK!!!")
-    # use streamlit?
+    st.title("Welcome to the MASTER COOK BOOK!!!")
+
+    st.button("Home")
+    st.button("Recipes")
+    st.button("Gallery")
+    st.button("Settings")
